e46ab8209a75544c23e391bfb7fd9028f97ec05e
Niels van Hagen <Niels.vanHagen@hacoustoprotec.com>
2020-09-17 15:47:15 +0200

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ void setup() {
   Serial.begin(9600);
    while (!Serial) {
      ;}
-     
-  //Serial.println("NADD test system version: 1.0");
+
+  Serial.println("NADD test system version: 1.0");
 
   Serial.println("Starting sd card test...");
   sdCardTest(); //sd card function

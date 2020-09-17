dbb7f035eb459041036c25bf3cff16fe6c695981
Niels van Hagen <Niels.vanHagen@hacoustoprotec.com>
2020-09-17 15:32:37 +0200

    commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "sdTest.h"
+
+void setup() {
+  Serial.begin(9600);
+   while (!Serial) {
+     ;}
+     
+  Serial.println("NADD test system version: 1.0");
+
+  Serial.println("Starting sd card test...");
+  sdCardTest(); //sd card function
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ src/sdTest.h
@@ -0,0 +1,7 @@
+#ifndef __sdTest_H__
+#define __sdTest_H__
+#include <Arduino.h>
+
+void sdCardTest();
+
+#endif

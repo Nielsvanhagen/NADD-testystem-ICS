8feade2d3d96f0159caba139635a8c6fb5187cab
Niels van Hagen <niels.vanhagen@hacoustoprotec.com>
2020-10-02 09:55:25 +0200

    Definition IO pins

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef __main_H__
+#define __main_H__
+
+#define PIN_GPO1	14				//light in push to talk button
+#define PIN_GPO2	39				//TBD
+#define PIN_GPO3	36				//TBD
+
+#define SOFTWARE_VERSION	"1.0"
+
+#endif

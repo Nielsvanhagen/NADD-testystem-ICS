85150652582e4b24eea64ecc1a7e027059efebe5
Niels van Hagen <niels.vanhagen@hacoustoprotec.com>
2020-10-02 09:57:41 +0200

    Input pinMode set

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,14 @@
 #include "ioTest.h"
 // #include "eeprom.h"
 
-void eepromTest();
-#define PIN_GPO1	14				//light in push to talk button
-#define PIN_GPO2	39				//TBD
-#define PIN_GPO3	36				//TBD
-
 void setup() {
   Serial.begin(9600);
    while (!Serial) {
      ;}
+
+     pinMode(PIN_GPI1, INPUT_PULLUP);
+   	 pinMode(PIN_GPI2, INPUT_PULLUP);
+   	 pinMode(PIN_GPI3, INPUT_PULLUP);
 
      pinMode(PIN_GPO1, OUTPUT);
      pinMode(PIN_GPO2, OUTPUT);
@@ -38,9 +37,6 @@ void loop() {
     else if(serIn=='3') {
       delay(500);
       Serial.println("Starting IO test...");
-      digitalWrite(PIN_GPO1, HIGH);
-      delay(500);
-      digitalWrite(PIN_GPO1, LOW);
       ioTest();
     }
   }
